1ca0881193edfad669ae6ea55b2a49868610530e
kumano <kumano@multisoup.co.jp>
2018-10-05 14:55:28 +0900

    モデルクラスを作ってみた

--- django_app/hello/models.py
+++ django_app/hello/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 # モデルに関する処理を記述するもの
 # Create your models here.
+
+class Friend(models.Model):
+    name = models.CharField(max_length=100)
+    mail = models.EmailField(max_length=200)
+    gender = models.BooleanField(default=0)
+    birthday = models.DateField()
+    
+    # __str__ テキストの値を返す為のもの
+    def __str__(self):
+        return '<Friend:id=' + str(self.id) + ',' + self.name + '(' + str(self.age) + ')>'
