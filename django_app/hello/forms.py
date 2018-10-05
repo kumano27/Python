8f46295a663c2b4496fb4728beeb1870b77ef8d5
kumano <kumano@multisoup.co.jp>
2018-10-05 11:28:42 +0900

    ラジオボタンを使ってみた

--- django_app/hello/forms.py
+++ django_app/hello/forms.py
@@ -4,8 +4,11 @@ from django import forms
 
 class HelloForm(forms.Form):
     data = [
-                ('one', 'item 1'),
-                ('two', 'item 2'),
-                ('three', 'item 3')
+                ('one', 'radio 1'),
+                ('two', 'radio 2'),
+                ('three', 'radio 3')
             ]
-    choice = forms.ChoiceField(label='Choice', choices=data)
+    # RadioSelect -> ウィジェット(Webページに表示されるHTMLタグをかんるするクラス)
+    # 今回の例だと widget 引数に RadioSelect を設定
+    choice = forms.ChoiceField(label='radio', \
+                               choices=data, widget=forms.RadioSelect())
