9e8dde3e4ef92a408a46db4bb8eac4aba97673ad
kumano <kumano@multisoup.co.jp>
2018-10-05 11:36:28 +0900

    リスト形式での表示を使ってみた

--- django_app/hello/forms.py
+++ django_app/hello/forms.py
@@ -4,11 +4,12 @@ from django import forms
 
 class HelloForm(forms.Form):
     data = [
-                ('one', 'radio 1'),
-                ('two', 'radio 2'),
-                ('three', 'radio 3')
+                ('one', 'item 1'),
+                ('two', 'item 2'),
+                ('three', 'item 3'),
+                ('four', 'item 4'),
+                ('five', 'item 5'),
             ]
-    # RadioSelect -> ウィジェット(Webページに表示されるHTMLタグをかんるするクラス)
-    # 今回の例だと widget 引数に RadioSelect を設定
+
     choice = forms.ChoiceField(label='radio', \
-                               choices=data, widget=forms.RadioSelect())
+                               choices=data, widget=forms.Select(attrs={'size': 5}))
