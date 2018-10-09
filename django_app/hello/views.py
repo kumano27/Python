1c1a47c29e81c351c117aab83b26e56acee4ec70
kumano <kumano@multisoup.co.jp>
2018-10-09 12:20:05 +0900

    IDを指定して取り出してみた

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -3,12 +3,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Friend
+from .forms import HelloForm
 
 def index(request):
-    data = Friend.objects.all()
     params = {
                 'title': 'Hello',
                 'message': 'all friends.' ,
-                'data': data,
+                'form': HelloForm(),
+                'data': [],
             }
+    # POSTされたかを判定
+    if(request.method == 'POST'):
+        # フォームから送られた値を元に指定のIDレコードをモデルインスタンスとして取り出す
+        num=request.POST['id']
+        item = Friend.objects.get(id=num)   # IDの値がnumのレコードを1つだけ取り出す
+        params['data']=[item]
+        params['form']=HelloForm(request.POST)
+    else:
+        params['data']=Friend.objects.all()
     return render(request, 'hello/index.html' , params)
