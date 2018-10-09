9ff13ba5ed6d0f8220bebef46c00416a310ed125
kumano <kumano@multisoup.co.jp>
2018-10-09 13:23:37 +0900

    QuerySetの表示をカスタマイズしてみた

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -3,15 +3,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Friend
+from django.db.models import QuerySet
+
+def __new_str__(self):
+    result = ''
+    for item in self:
+        result += '<tr>'
+        for k in item:
+            result += '<td>' + str(k) + '=' + str(item[k]) + '</td>'
+        result += '</tr>'
+    return result
+        
+QuerySet.__str__ = __new_str__
 
 def index(request):
-    # レコード数
-    num = Friend.objects.all().count()
-    # 最初のレコードを返す
-    first = Friend.objects.all().first()
-    # 最後のレコードを返す
-    last = Friend.objects.all().last()
-    data = [num, first, last]
+    data = Friend.objects.all().values('id','name','age')
     params = {
                 'title': 'Hello',
                 'data': data,
