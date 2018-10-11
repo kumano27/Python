2edb62b16e90fa40bdb2f57ca6172401b65376b2
kumano <kumano@multisoup.co.jp>
2018-10-11 18:57:05 +0900

    モデルで使えるバリデーターをまとめてみた

--- django_app/hello/models.py
+++ django_app/hello/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 # モデルに関する処理を記述するもの
 # Create your models here.
 
 class Friend(models.Model):
-    name = models.CharField(max_length=100)
-    mail = models.EmailField(max_length=200)
+    # 最小文字数10文字以内だとエラー
+    name = models.CharField(max_length=100,validators=[MinLengthValidator(10)])
+    # 最小文字数10文字以内だとエラー
+    mail = models.EmailField(max_length=200,validators=[MinLengthValidator(10)])
     gender = models.BooleanField()
-    age = models.IntegerField(default=0)
+    age = models.IntegerField()
     birthday = models.DateField()
     
     # __str__ テキストの値を返す為のもの
