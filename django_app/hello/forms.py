438daae8ca0273d6be70eda4d7edbd3855fbe6a7
kumano <kumano@multisoup.co.jp>
2018-10-11 15:42:18 +0900

    min_length、max_length試してみた

--- django_app/hello/forms.py
+++ django_app/hello/forms.py
@@ -24,4 +24,12 @@ class FindForm(forms.Form):
     find = forms.CharField(label='Find', required=False)
     
 class CheckForm(forms.Form):
-    str = forms.CharField(label='Name')
+    # 空の入力を許可するか指定
+    empty = forms.CharField(label='Empty', empty_value=True)
+    
+    # 入力するテキストの最小文字数を指定
+    # 以下の例だと10文字以上を入力する必要がある
+    min = forms.CharField(label='Min', min_length=10)
+    # 入力するテキストの最大文字数を指定
+    # 以下の例だと10文字以下の入力のみ受け付ける
+    max = forms.CharField(label='Max', max_length=10)
