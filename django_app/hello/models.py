4e7f42d99b0709dd0e0368209c49498123c7d70a
kumano <kumano@multisoup.co.jp>
2018-10-12 10:30:43 +0900

    バリデータ関数を作ってみた

--- django_app/hello/models.py
+++ django_app/hello/models.py
@@ -1,16 +1,24 @@
+# re -> Python正規化モジュール
+import re
 from django.db import models
-from django.core.validators import RegexValidator
+from django.core.validators import ValidationError
 # モデルに関する処理を記述するもの
 # Create your models here.
 
+def number_only(value):
+    # 正規表現モジュールと引数に指定したテキストのパターンがマッチングするか？
+    if(re.match(r'^[0-9]*$', value) == None):
+        # マッチングしたら...
+        raise ValidationError(
+                    '%(value)s is not Number!', params={'value': value},
+                )
+
 class Friend(models.Model):
-    # a～zの小文字だけを入力許可
-    name = models.CharField(max_length=100,validators=[RegexValidator(r'^[a-z]*$')])
+    name = models.CharField(max_length=100,validators=[number_only])
     mail = models.EmailField(max_length=200)
     gender = models.BooleanField()
     age = models.IntegerField()
     birthday = models.DateField()
     
-    # __str__ テキストの値を返す為のもの
     def __str__(self):
         return '<Friend:id=' + str(self.id) + ',' + self.name + '(' + str(self.age) + ')>'
