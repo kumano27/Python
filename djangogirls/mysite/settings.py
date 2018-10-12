c7e218dd3daa80cb7e746b12ae86c8ffa9d3034a
kumano <kumano@multisoup.co.jp>
2018-10-12 16:15:00 +0900

    DBの設定、http:127.0.0.1:8000/へのアクセス成功を確認

--- djangogirls/mysite/settings.py
+++ djangogirls/mysite/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myapp',
+        'USER': 'postgres',
+        'PASSWORD': 'ms0420',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        
     }
 }
 
