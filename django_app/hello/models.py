fd557b676add9f51101b97b0dd4a2c6375d2feb3
kumano <kumano@multisoup.co.jp>
2018-10-12 10:05:08 +0900

    正規表現パターンを使ったバリデーションを試してみた(RegexValidator)

--- django_app/hello/models.py
+++ django_app/hello/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-from django.core.validators import URLValidator
+from django.core.validators import RegexValidator
 # モデルに関する処理を記述するもの
 # Create your models here.
 
 class Friend(models.Model):
-    # 最小文字数10文字以内だとエラー
-    name = models.CharField(max_length=100,validators=[URLValidator()])
-    # 最小文字数10文字以内だとエラー
+    # a～zの小文字だけを入力許可
+    name = models.CharField(max_length=100,validators=[RegexValidator(r'^[a-z]*$')])
     mail = models.EmailField(max_length=200)
     gender = models.BooleanField()
     age = models.IntegerField()
