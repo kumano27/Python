492a7abab5d2e0db7415aea3c2944d1902cdc324
kumano <kumano@multisoup.co.jp>
2018-10-15 14:45:59 +0900

    テーブルにサンプル値入力まで

--- pokemon/mysite/models.py
+++ pokemon/mysite/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Squirtle(models.Model):
+    CP = models.IntegerField()
+    HP = models.IntegerField()
+    SEX = models.BooleanField()
+    WEIGHT = models.FloatField()
+    HEIGHT = models.FloatField()
+    MOVE1 = models.CharField(max_length=10)
+    MOVE2 = models.CharField(max_length=10)
+    
+    def __str__(self):
+        return '<Squirtle:CP=' + str(self.CP) + '>'
--- pokemon/mysite/views.py
+++ pokemon/mysite/views.py
@@ -1,6 +1,11 @@
 # Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Squirtle
 
 def index(request):
-    return render(request, 'mysite/index.html')
+    data = Squirtle.objects.all()
+    params = {
+                'data': data,
+            }
+    return render(request, 'mysite/index.html',params)
